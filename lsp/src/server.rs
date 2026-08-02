80b99ca47ed1baa4b6c6f43d532adea4ca58c06a
Johannes Asal <johannes.asal@gmail.com>
2026-08-02 21:29:52 +0200

    Make lsp inherit version from workspace

--- lsp/src/server.rs
+++ lsp/src/server.rs
@@ -71,7 +71,7 @@ impl LanguageServer for QueLanguageServer {
             },
             server_info: Some(ServerInfo {
                 name: "que-lsp".to_string(),
-                version: Some("0.1.0".to_string()),
+                version: Some(env!("CARGO_PKG_VERSION").to_string()),
             }),
         })
     }
